a981eefcd76a5be43a49d0b6c945c0b9de0a03b3
alexanderbkl <asederado@gmail.com>
2023-02-28 04:37:43 -0500

    cambios

--- init.lua
+++ init.lua
@@ -289,14 +289,14 @@ vim.keymap.set('n', '<leader>/', function()
   })
 end, { desc = '[/] Fuzzily search in current buffer' })
 --NeoTreeShowToggle when press <leader>o (Explorer):
-vim.keymap.set('n', '<leader>e', ':NeoTreeShowToggle<CR>', { desc = '[E]xplorer' })
+vim.keymap.set('n', '<leader>o', ':NeoTreeShowToggle<CR>', { desc = '[O]xplorer' })
 vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch the [F]iles' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 
--- [[ Configure Treesitter ]]
+-- [[ Configure Treesitter ]O
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
